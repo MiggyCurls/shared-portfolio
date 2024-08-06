42420ba7d21a9bdfb28bdc290269cd6987fff84c
miguel <migpromohulk@gmail.com>
2024-08-05 21:35:04 -0400

    add calculator functionality and neat css

--- CalculatorProject/calcScript.js
+++ CalculatorProject/calcScript.js
@@ -18,11 +18,47 @@ function delButt(){
     display.value = text.substring(0, text.length-1);
 }
 
+function calculate(){
+    display.value = eval(display.value);
+}
+
 function addDecimal(){
     const text = display.value;
-    if (text.indexOf(".") == -1){
-        //there is no decimal on screen, free to add one
-        display.value += ".";
+    let canBeAdded = true;
+    
+    //Between operands you can only have one decimal
+    for(let i = 0; i < text.length; i++){
+        //Boolean flips every time it finds operand or decimal
+        if(text.charAt(i) == '.'){
+            canBeAdded = false;
+        }
+        if(text.charAt(i) == '+' || text.charAt(i) == '/' || text.charAt(i) == '*' || text.charAt(i) == '-'){
+            canBeAdded = true;
+        }
+    }
+    if(canBeAdded){
+        display.value += '.';
+    }
+
+}
+
+function addParenthesis(){
+    const text = display.value;
+    let leftP = 0;
+
+    for(let i = 0; i < text.length; i++){
+        if(text.charAt(i) == '('){
+            leftP++;
+        }else if(text.charAt(i) == ')'){
+            leftP--;
+        }
+    }
+    const str = text.slice(text.length-1, text.length);
+    //Check to see if last character is a number and apply/don't apply correct parenthesis
+    if(leftP == 0 && (text.at(-1) != ')' && str.search(/^[0-9]+$/) == -1)){
+        display.value += '(';
+    }else if (leftP != 0 && (text.at(-1) != '(' && str.search(/^[0-9]+$/) != -1)){
+        display.value += ')';
     }
 }
 
@@ -40,8 +76,11 @@ function signs(){
 
 function operation(input){
     const text = display.value.at(-1);
-   
-    if(text != '+' && text != '-' && text != '/' && text != '*'){
+
+    if(input == '-' && text == '('){
+        //A minus operation can be interpreted as a negative value
+        display.value += input;
+    }else if(text != '+' && text != '-' && text != '/' && text != '*' && text != '('){
         //operand is not at end of screen
         display.value += input;
     }

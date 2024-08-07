0cf4f5d9431e704c83b890f7388d582b400a6ec5
miguel <migpromohulk@gmail.com>
2024-08-07 16:46:50 -0400

    add login application proj info to web page

--- clique.js
+++ clique.js
@@ -5,13 +5,30 @@
 */
 
 window.onload = function(){
-    const div = document.getElementById('calcDiv');
-    projectOnHover(div);
+    const projList = document.querySelectorAll('.projects');
+    for(let i = 0; i < projList.length; i++){
+        projList[i].addEventListener("mouseover", () => {
+            for(let j = 0; j < projList.length; j++){
+                if(projList[j] != projList[i]){
+                    projList[j].classList.add("not-hovered");
+                }else{
+                    projList[j].style = 'box-shadow: 0px 10px 50px 5px grey';
+                    projList[j].style.transform = 'scale(1.1)';
+                    projList[j].style.transition = 'transform 1.05s ease-in-out';
+                }
+            }
+        });
+        projList[i].addEventListener("mouseout", () => {
+            for(let j = 0; j < projList.length; j++){
+                projList[j].classList.remove("not-hovered");
+                projList[j].style = 'box-shadow:none';
+            }
+        })
+    }
 }
 function projectOnHover(div){
     const others = document.getElementById('notProgress');
     div.onmouseover = function(){
-        console.log('mouse over image');
         div.style = 'box-shadow: 0px 10px 50px 5px grey';
         //slowly phase in shadow
         //Look into keyframes to have shadow transition at same time as scale transform
@@ -20,7 +37,6 @@ function projectOnHover(div){
         others.style.opacity = 0.5;
     };
     div.onmouseout = function(){
-        console.log('mouse not over image');
         div.style.transform = 'none';
         div.style = 'box-shadow:none';
         others.style.opacity = 1;

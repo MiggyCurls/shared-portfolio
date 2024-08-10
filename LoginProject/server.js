960082f913905ffb5e37b5bf000f9ac33f36e4a9
miguel <migpromohulk@gmail.com>
2024-08-09 22:47:06 -0400

    add password protection

--- LoginProject/server.js
+++ LoginProject/server.js
@@ -3,7 +3,18 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const port = 8080;
-
+const path = require('path');
+//text file to hold username and passwords
+const fs = require('fs');
+fs.readFile('public/registry.txt','utf-8', (err, data) =>{
+    if(err){
+        console.error(err);
+        return;
+    }
+    if(data.length == 0){
+        console.log('no data present :(');
+    }
+});
 app.use(bodyParser.json());
 app.use(express.static('public'));
 app.use((req, res, next) => {
@@ -13,15 +24,35 @@ app.use((req, res, next) => {
     next();
 });
 let users = [];
-
+let data = "";
 app.get('/', (req, res) =>{
-    res.send(users);
+    res.sendFile(path.join(__dirname,'public','registry.txt'));
 });
 app.post('/', (req, res) =>{
     res.status(200).send({status: 'received'});
     const body = req.body;
-    users.push({email: body.email, username: body.username, password: body.password});
+    users.push({email: body.email, username: body.username, password: body.mdHash});
+    //Separate each JSON object in users with a new line
+    data = makePretty(JSON.stringify(users));
+    //Look into append to file after day one of putting into production
+    fs.writeFile('public/registry.txt', data, err =>{
+        if(err){
+            console.error(err);
+        }
+    });
 });
+
 app.listen(port, () =>{
     console.log(`server running at : ${port}`);
 });
+
+function makePretty(data){
+    let pretty = "";
+    for(let i = 0; i < data.length; i++){
+        pretty += (data[i]);
+        if(data[i] == '}'){
+            pretty += '\n';
+        }
+    }
+    return pretty;
+}

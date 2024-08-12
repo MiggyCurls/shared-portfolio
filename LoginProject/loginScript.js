f60216ad58dd1b121e835d949db12f032ca9d8dd
miguel <migpromohulk@gmail.com>
2024-08-11 23:38:01 -0400

    add login functionality

--- LoginProject/loginScript.js
+++ LoginProject/loginScript.js
@@ -1,4 +1,36 @@
+/*
+    Login to verify user information will be sent
+    to server to check credentials
+*/
+
+document.getElementById('loginForm').addEventListener('submit', async function login(event){
+    event.preventDefault();
+    const formType = 'login';
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    try{
+        const response = await fetch('http://localhost:8080/', {
+            method: 'POST',
+            headers: {
+                'Content-Type':'application/json'
+            },
+            body:JSON.stringify({formType, username, password})
+        });
+        if(response.status == 200){
+            console.log('User has an account and is logged in :)');
+            alert('You have logged in to your account :)');
+            //redirect users to some place perhaps??
+        }else{
+            const str = 'No account found with username: ' + username;
+            document.getElementById('noAccountFound').innerHTML = str;
+        }
+    }catch(error){
+        console.error("failed poorly ", error);
+    }
+});
 
 function goBack(){
     window.location.href = "../index.html";
 }
+
